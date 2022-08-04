62ff89337e604205e2af1bb5cd33502c78a01b8b
LinYiMing <13975073750@163.com>
2022-08-04 11:19:36 +0800

    用户页面完成

--- assets/js/basedAPI.js
+++ assets/js/basedAPI.js
@@ -6,5 +6,23 @@
 const baseUrl = 'http://www.liulongbin.top:3007'
 
 $.ajaxPrefilter(option => {
+  // 统一为有权限的接口，设置 headers 请求头
+  if (option.url.includes('/my/')) {
+    option.headers = {
+      Authorization: localStorage.getItem('token'),
+    }
+  }
   option.url = baseUrl + option.url
+
+  // 不论成功还是失败，最终都会调用 complete 回调函数
+  option.complete = res => {
+    console.log(res)
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      //  强制清空 token
+      localStorage.removeItem('token')
+      // 强制跳转到登录页面
+      location.href = '/login.html'
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+const getUserInfo = () => {
+  $.ajax({
+    type: 'GET',
+    url: '/my/userinfo',
+    data: null,
+    success: res => {
+      //   console.log(res)
+      const { status, message } = res
+      if (status !== 0) return layui.layer.msg('数据请求失败！')
+      // 否则，调用 renderAvatar 渲染用户头像
+      renderAvater(res.data)
+    },
+  })
+}
+
+const renderAvater = data => {
+  let name = data.nickname || data.username
+  // 设置欢迎文本
+  $('#welcome').html(`欢迎 ${name}`)
+  // 按需渲染用户头像
+  if (data.user_pic !== null) {
+    // 渲染图片头像
+    $('.layui-nav-img').attr('src', data.user_pic).show()
+    $('.text-avatar').hide()
+  } else {
+    // 渲染文本头像
+    $('.layui-nav-img').hide()
+    let firstName = name[0].toUpperCase()
+    $('.text-avatar').html(firstName)
+  }
+}
+getUserInfo()
+
+// 退出登录
+$('#btnLogout').click(() => {
+  layui.layer.confirm('确定退出登录？', { icon: 3, title: '' }, function (index) {
+    // 清空本地存储里面的 token
+    localStorage.removeItem('token')
+    // 重新跳转到登录页面
+    location.href = '/login.html'
+  })
+})
